cf85179b87b6360c28fbedd8031d8f607d66207b
onepiga <646998823@qq.com>
2021-02-06 17:09:23 +0800

    修改storeage

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,15 @@
-// module.exports = {
-//   devServer: {
-//     host: 'localhost',
-//     port: 8080,
-//     proxy: {
-//       '/api': {},
-//     },
-//   },
-// }
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'https://www.imooc.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '/api': '',
+        },
+      },
+    },
+  },
+}
