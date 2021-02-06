b8a99d0c537ec55fc5f288ec2a992fc5469c7c83
onepiga <646998823@qq.com>
2021-02-06 19:46:57 +0800

    header组件

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,6 +1,42 @@
 <template>
-  <div>
-    NavHeader
+  <div class="header">
+    <div class="nav-topbar">
+      <div class="container">
+        <div class="topbar-menu">
+          <a href="javascript:;">小米商城</a>
+          <a href="javascript:;">MUI</a>
+          <a href="javascript:;">云服务</a>
+          <a href="javascript:;">协议规则</a>
+        </div>
+        <div class="topbar-user">
+          <a href="javascript:;">登入</a>
+          <a href="javascript:;">注册</a>
+          <a href="javascript:;" class="my-cart"><span class="icon-cart"></span>购物车</a>
+        </div>
+      </div>
+    </div>
+    <div class="nav-header">
+      <div class="container">
+        <div class="header-logo">
+          <a href="#"></a>
+        </div>
+        <div class="header-menu">
+          <div class="item-menu">
+            <span>小米手机</span>
+            <div class="childern"></div>
+          </div>
+          <div class="item-menu">
+            <span>红米手机</span>
+            <div class="childern"></div>
+          </div>
+          <div class="item-menu">
+            <span>电视</span>
+            <div class="childern"></div>
+          </div>
+        </div>
+        <div class="header-search"></div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -8,3 +44,95 @@ export default {
   name: 'nav-header',
 }
 </script>
+<style scoped lang="scss">
+@import '~@/assets/scss/base.scss';
+.header {
+  .nav-topbar {
+    height: 39px;
+    line-height: 39px;
+    background-color: #333333;
+    color: #b0b0b0;
+    .container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      a {
+        display: inline-block;
+        color: #b0b0b0;
+        margin-right: 17px;
+      }
+      .my-cart {
+        width: 110px;
+        background-color: #ff6600;
+        text-align: center;
+        color: #ffffff;
+        .icon-cart {
+          display: inline-block;
+          width: 16px;
+          height: 12px;
+          background: url('/imgs/icon-cart-checked.png') no-repeat center;
+          background-size: contain;
+          margin-right: 4px;
+        }
+      }
+    }
+  }
+  .nav-header {
+    .container {
+      height: 112px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .header-logo {
+        display: inline-block;
+        width: 55px;
+        height: 55px;
+        background-color: #ff6600;
+        a {
+          display: inline-block;
+          width: 110px;
+          height: 55px;
+          &::before {
+            content: '';
+            display: inline-block;
+            width: 55px;
+            height: 55px;
+            background: url('/imgs/mi-home.png') no-repeat center;
+            background-size: 55px;
+            transition: margin-left 0.2s;
+          }
+          &::after {
+            content: '';
+            display: inline-block;
+            width: 55px;
+            height: 55px;
+            background: url('/imgs/mi-logo.png') no-repeat center;
+            background-size: 55px;
+          }
+          &:hover::before {
+            margin-left: -55px;
+            transition: margin-left 0.2s;
+          }
+        }
+      }
+      .header-menu {
+        display: inline-block;
+        width: 200px;
+        padding-left: 209px;
+        .item-menu {
+          display: inline-block;
+          color: #333333;
+          font-weight: bold;
+          font-size: 16px;
+          line-height: 112px;
+          span {
+            cursor: pointer;
+          }
+          &:hover {
+          }
+        }
+      }
+    }
+  }
+}
+</style>
