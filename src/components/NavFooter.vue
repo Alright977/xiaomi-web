f665fcdff3d35c4600df50ecaee9ea9797d737f5
onepiga <646998823@qq.com>
2021-02-07 14:42:02 +0800

    footer组件

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,8 +1,49 @@
 <template>
-  <div> </div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="" />
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link"><a href="" target="_blank">MUUI</a><span>|</span> <a href="" target="_blank">米家 </a><span>|</span> <a href="" target="_blank">米聊</a><span>|</span> <a href="" target="_blank">小米天猫店</a><span>|</span> </div>
+    <div class="copyright">Copyright @2020 <span class="domain">mi.futurefe.com</span> All Rights Reserved</div>
+  </div>
 </template>
 <script>
 export default {
   name: 'nav-footer',
 }
 </script>
+<style scoped lang="scss">
+.footer {
+  height: 234px;
+  border-top: 4px solid #ff6600;
+  background-color: #333333;
+  color: #999999;
+  font-size: 16px;
+  text-align: center;
+  .footer-logo {
+    margin-top: 46px;
+    margin-bottom: 31px;
+    img {
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link {
+    a {
+      color: #999999;
+      display: inline-block;
+    }
+    span {
+      margin: 0 10px;
+    }
+    margin-top: 13px;
+  }
+  .copyright {
+    .domain {
+      color: #ff6600;
+    }
+  }
+}
+</style>
