7775c559787bc633e70079b98d5dfe8583d22cbb
onepiga <646998823@qq.com>
2021-02-07 14:19:46 +0800

    baseurl

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 import axios from './https'
 
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 new Vue({
